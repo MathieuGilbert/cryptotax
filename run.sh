6da7a1d62bb0e7d6c448397d6ecdd57004fb2414
Mathieu Gilbert <matnarak@gmail.com>
2018-08-04 18:21:23 -0600

    Added comments in run script for other processes that might need to be running.

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# sass --watch --sourcemap=none web/css/styles.scss:web/css/styles.css
+# ~/src/mailslurper-1.14.1-osx/mailslurper
 go run cryptotax.go helpers.go handlers.go middleware.go
